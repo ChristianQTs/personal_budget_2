c36fea4deb917259e38b7891e6e1ee62af90fc85
christian <christian.cuneo@libero.it>
2026-02-06 17:40:24 +0100

    Fixed Envelope Import {}

--- envelopeRoutes.js
+++ envelopeRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import {Envelope} from './db.js'
 import { getEnvNames, getAllEnvs, getSingleEnv, createEnv, transferBudget, withdrawFromEnv, deleteEnv } from './envelopeControllers.js'
 
 const envRouter = express.Router()
 
 //check envelope exists by envelope name
 envRouter.param('envName', async (req, res, next, envName) => {
-    const envelope = await Envelope.findOne({ where: { name: envName } })
+    const envelope = await Envelope.findOne({ where: { name: envName.toLowerCase() } })
     if (!envelope) {
         const err = new Error('Envelope does not exist.')
         err.status = 404
@@ -20,8 +21,8 @@ envRouter.get('/names', getEnvNames)
 envRouter.get('/', getAllEnvs)
 envRouter.get('/:envName', getSingleEnv)
 envRouter.post('/', createEnv)
-envRouter.put('/:from/:to', transferBudget)
 envRouter.put('/:envName/withdraw', withdrawFromEnv)
+envRouter.put('/:from/:to', transferBudget)
 envRouter.delete('/:envName', deleteEnv)
 
 export  {envRouter}
