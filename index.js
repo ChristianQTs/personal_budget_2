abf194eda993cd20a91699ce198148a45f97d3f6
christian <christian.cuneo@libero.it>
2026-02-12 14:51:45 +0100

    Moved PORT to env file

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.json())
 import { envRouter } from './envelopeRoutes.js'
 import { transactionRouter } from './transactionRoutes.js'
 
+const PORT = process.env.PORT || 3000
+
+
+
 app.use('/personalBudget', envRouter)
 app.use('/personalBudget', transactionRouter)
 
@@ -14,4 +18,4 @@ app.use((err, req, res, next) => {
     const status = err.status || 500
     res.status(status).json({ message: `Error ${status} : ${err.message}` })
 })
-app.listen(3000,'0.0.0.0', () => console.log('Server listening on port 3000...'))
+app.listen(PORT,'0.0.0.0', () => console.log('Server listening on port 3000...'))
