4c5d738f8f8739532570b07a6b77cd3e87685f36
christian <christian.cuneo@libero.it>
2026-02-14 15:46:13 +0100

    Fixed bugs

--- public/mainpage.js
+++ public/mainpage.js
@@ -1,27 +1,32 @@
-const allEnvButton = document.getElementById('allEnvsBTN')
+const fetchAllEnvelopes = async () => {
+    const URL = `${window.location.origin}/personalBudget/envelopes`
 
-const fetchAllEnvs = async () => {
-    const URL = window.location.origin + '/personalBudget/envelopes'
-    const resDiv = document.createElement('div')
-    document.body.appendChild(resDiv)
-    try {
-        const response = await fetch(URL)
-        const json = await response.json()
-        if (response.ok) {
-            for (let env = 0; env < json.length; env++) {
-                const envelope = document.createElement('div')
-                for (let field in json[env]) {
-                    const newLine = document.createElement('h2')
-                    newLine.textContent = `${field} : ${json[env][field]}`
-                    envelope.appendChild(newLine)
+        const resDiv = document.createElement('div')
+        document.body.appendChild(resDiv)
+        try {
+            const response = await fetch(URL)
+            const json = await response.json()
+
+            if (response.ok) {
+                for (let env of json) {
+                    const envelope = document.createElement('div')
+                    for (let field in env) {
+                        const newLine = document.createElement('h2')
+                        newLine.textContent = `${field} : ${env.field}`
+                        envelope.appendChild(newLine)
+                    }
+                    resDiv.appendChild(envelope)
                 }
-                resDiv.appendChild(envelope)
+            } else {
+                window.alert(json.message)
             }
-        } else {
-            window.alert(json.message)
+        } catch (err) {
+            window.alert(err.message)
         }
-    } catch (err) {
-        window.alert(err)
     }
-}
-allEnvButton.addEventListener('click', fetchAllEnvs)
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const getAllEnvsButton = document.getElementById('allEnvsBTN')
+    getAllEnvsButton.addEventListener('click', fetchAllEnvelopes)
+})
